0db5e225b5816459c40ad8a58de0dc22d34312f4
Siim-byte <beloimiis@gmail.com>
2025-10-05 20:56:27 +0300

    alr

--- Belõi/harjutused/muusika/muusikaScript.js
+++ Belõi/harjutused/muusika/muusikaScript.js
@@ -40,38 +40,99 @@ function tundiMuusika(){
     vastus3.innerHTML="Sa kuulad muusikat  "+tundi.value+"  tundi päevas";
     vastus3.style.color="red";
 }
-function radioValik(){
-    let yes=document.getElementById('yes');
-    let no=document.getElementById('no');
-    let vastus4=document.getElementById('vast4');
+function radioValik() {
+    let ja = document.getElementById('ja');
+    let ei = document.getElementById('ei');
+    let vastus4 = document.getElementById('vastus4');
 
-    if(yes.checked){
-        vastus4.innerHTML="Raadio kuulamine: Jah";
-        vastus4.style.color="red";
+    if (ja.checked) {
+        vastus4.innerHTML = "Raadio kuulamine: Jah";
+    } else if (ei.checked) {
+        vastus4.innerHTML = "Raadio kuulamine: Ei";
+    } else {
+        vastus4.innerHTML = "Tee oma valiku!";
     }
-    else if(no.checked){
-        vastus4.innerHTML="Raadio kuulamine: Ei";
-        vastus4.style.color="red";
-    }
-    else{
-        vastus4.innerHTML="Tee oma valiku!";
-        vastus4.style.color="red";
-    }
+
+    vastus4.style.color = "red";
 }
-function puhasta(){
-    let abba=document.getElementById('abba');
-    let the=document.getElementById('the');
-    let system=document.getElementById('system');
-    let lemon=document.getElementById('lemon');
-    let vastus=document.getElementById('vastus');
+function nimetatudJaamad() {
+    let jaamad = document.getElementById('nimetada').value;
+    let vastus5 = document.getElementById('vastus5');
 
-    abba.checked=false;
-    the.checked=false;
-    system.checked=false;
-    lemon.checked=false;
-    vastus.innerText="";
+    if (jaamad.trim() !== "") {
+        vastus5.innerHTML = "Sinu nimetatud jaamad: " + jaamad;
+    } else {
+        vastus5.innerHTML = "";
+    }
+    vastus5.style.color = "red";
+}
+function muusikaStiilSelect() {
+    let muusika = document.getElementById('muusika').value;
+    let vastus6 = document.getElementById('vastus6');
+
+    if (muusika !== "") {
+        vastus6.innerHTML = "Sinu vastus: " + muusika;
+    } else {
+        vastus6.innerHTML = "Palun vali muusikastiil!";
+    }
+    vastus6.style.color = "red";
+}
+
+function puhasta(){
+    document.getElementById('abba').checked=false;
+    document.getElementById('the').checked=false;
+    document.getElementById('system').checked=false;
+    document.getElementById('lemon').checked=false;
+    document.getElementById('vastus').innerText="";
+
+    document.getElementById('arvamus').value="";
+    document.getElementById('vastus2').innerText="";
+
+    document.getElementById('tundi').value=0;
+    document.getElementById('vastus3').innerText="";
+
+    document.getElementById('ja').checked=false;
+    document.getElementById('ei').checked=false;
+    document.getElementById('vastus4').innerText="";
+
+    document.getElementById('nimetada').value="";
+    document.getElementById('vastus5').innerText="";
+
+    document.getElementById('muusika').value="";
+    document.getElementById('vastus6').innerText="";
+
+    document.getElementById('kokkuvote').innerHTML="";
 }
 function saada(){
-    let vastuskokku=document.getElementById('vastus');
-    vastuskokku.innerHTML=arvamus.value+", "+ valik.value+...;
+    let abba = document.getElementById('abba');
+    let the = document.getElementById('the');
+    let system = document.getElementById('system');
+    let lemon = document.getElementById('lemon');
+    let arvamus = document.getElementById('arvamus');
+    let tundi = document.getElementById('tundi');
+    let ja = document.getElementById('ja');
+    let ei = document.getElementById('ei');
+    let nimetada = document.getElementById('nimetada');
+    let muusika = document.getElementById('muusika');
+    let kokkuvote = document.getElementById('kokkuvote');
+
+    let valik = "";
+    if (abba.checked) valik += abba.value + ", ";
+    if (the.checked) valik += the.value + ", ";
+    if (system.checked) valik += system.value + ", ";
+    if (lemon.checked) valik += lemon.value + ", ";
+
+    let raadio = "";
+    if (ja.checked) raadio = ja.value;
+    if (ei.checked) raadio = ei.value;
+
+    kokkuvote.innerHTML =
+        "Muusikud: " + valik +""+
+        "Arvamus: " + arvamus.value + ", " +
+        "Kuulad muusikat: " + tundi.value + " tundi päevas, " +
+        "Raadio kuulamine: " + raadio + ", " +
+        "Raadiojaamad: " + nimetada.value + ", " +
+        "Lemmikmuusika: " + muusika.value;
+
+    kokkuvote.style.color = "red";
 }
